89c4597892c3b40873d40517040ab2a7610ec9ed
byeongrae-kim <rlaqudfo1719@gmail.com>
2026-07-31 09:50:40 +0900

    Delete src directory

--- src/main/java/com/ex/repository/CustomerOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ex.repository;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ex.entity.CustomerOrder;
-
-public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Long> {
-	List<CustomerOrder> findAllByOrderByCreatedAtDesc();
-}
